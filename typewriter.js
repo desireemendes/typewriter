9e727e497251701eb5d821bc463f561ffe47414a
Desiree Mendes <88469223+desireemendes@users.noreply.github.com>
2021-11-02 14:19:43 -0400

    Update typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -4,14 +4,20 @@ for (const char of sentence) setTimeout(() => {
 }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
 */
 
-const sentence = "hello there from lighthouse labs";
-let timer;
-let i = 0;
-function text () {
-  document.write(text[i]);
-  i++;
-  if (i > text.length)
-  clearInterval(timer);
-  timer = setInterval(type, 50);
-}
-console.log(sentence);
+// const sentence = "hello there from lighthouse labs";
+// let timer;
+// let i = 0;
+// function text () {
+//   document.write(text[i]);
+//   i++;
+//   if (i > text.length)
+//   clearInterval(timer);
+//   timer = setInterval(type, 50);
+// }
+// console.log(sentence);
+
+const sentence = "hello there from lighthouse labs\n";
+let delay = 1000
+for (const char of sentence) setTimeout(() => {
+  process.stdout.write(char)
+}, delay, delay += 500)
